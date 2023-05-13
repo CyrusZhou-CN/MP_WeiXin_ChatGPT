f53c0f35bde4305b150a296245d894179d61fd6a
CyrusZhou <lsqtzj@msn.com>
2023-05-13 19:18:16 +0200

    多语言跳转问题

--- middleware.ts
+++ middleware.ts
@@ -3,23 +3,25 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-    // Get the pathname of the request (e.g. /, /protected)
-    const path = req.nextUrl.pathname;
-    // If it's the root path, just render it
-    if (path === "/") {
-        return NextResponse.next();
-    }
-    const session = await getToken({
-        req,
-        secret: process.env.NEXTAUTH_SECRET,
-    });
-    console.log("path:", path);
-    console.log("session:", session);
+  // Get the pathname of the request (e.g. /, /protected)
+  const path = req.nextUrl.pathname;
+  const [language] = path.substr(1).split("/");
 
-    if (!session && /^\/admin(?!\/login)/.test(path)) {
-        return NextResponse.redirect(new URL("/auth/login", req.url));
-      } else if (session && /^\/admin\/?(?:login\/?)?$/.test(path)) {
-        return NextResponse.redirect(new URL("/admin/dashboard", req.url));
-      }
+  // If it's the root path, just render it
+  if (path === "/") {
     return NextResponse.next();
+  }
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  console.log("path:", path);
+  console.log("req:", req);
+
+  if (!session && /^\/admin(?!\/login)/.test(path)) {
+    return NextResponse.redirect(new URL(`/${language}/auth/login`, req.url));
+  } else if (session && /^\/admin\/?(?:login\/?)?$/.test(path)) {
+    return NextResponse.redirect(new URL(`/${language}/admin/dashboard`, req.url));
+  }
+  return NextResponse.next();
 }
