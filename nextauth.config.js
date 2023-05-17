b8c660fdacef045b0986575f1929eb3b25925b6e
CyrusZhou <lsqtzj@msn.com>
2023-05-17 18:38:36 +0200

    NEXTAUTH_URL 测试

--- nextauth.config.js
+++ nextauth.config.js
@@ -1,13 +1,12 @@
 // nextauth.config.js
-const baseUrl = process.env.VERCEL_URL || process.env.NEXTAUTH_URL || 'http://localhost:3000';
-console.log('baseUrl:', baseUrl);
+
 module.exports = {
     // ...其他配置项
     serverRuntimeConfig: {
-      NEXTAUTH_URL: baseUrl,
+      NEXTAUTH_URL: process.env.VERCEL_URL ,
     },
     env: {
-      NEXTAUTH_URL: baseUrl,
+      NEXTAUTH_URL: process.env.VERCEL_URL,
     },
   };
   
