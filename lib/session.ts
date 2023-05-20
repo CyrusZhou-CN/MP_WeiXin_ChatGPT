b2544d3cd0f0344865ba9528ac76cc268e4b03a5
CyrusZhou <lsqtzj@msn.com>
2023-05-20 10:13:44 +0200

    SECRET_COOKIE_PASSWORD 默认密码

--- lib/session.ts
+++ lib/session.ts
@@ -1,7 +1,7 @@
 import { IronSessionOptions } from 'iron-session';
 
 export const sessionOptions: IronSessionOptions = {
-  password: process.env.SECRET_COOKIE_PASSWORD || "123456" as string,
+  password: process.env.SECRET_COOKIE_PASSWORD || "f0842d676032c84ceab32453e9eed80c" as string,
   cookieName: "mp-weixin-chatgpt-iron-session",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
